dea59e2d7a8c3c90cb9011fd2eeb5448e166dea0
yqiu <120360845@qq.com>
2021-04-07 15:03:31 +0800

    Add shape.py

---
+++ starduster/shape.py
@@ -0,0 +1,17 @@
+from .utils import Evaluator
+
+import torch
+
+
+class Evaluator_Shape(Evaluator):
+    def __init__(self, model, opt):
+        super(Evaluator_Shape, self).__init__(model, opt, ("loss", "l"))
+
+
+    def loss_func(self, x, y, backward=True):
+        y_mu = y[:, :, 0]
+        y_std = y[:, :, 1]
+        y_pred = self.model(x)
+        delta = (y_pred - y_mu)/y_std
+        return torch.mean(delta*delta), None
+
