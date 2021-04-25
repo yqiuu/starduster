fa34e8b4dee73bad491bf41b93c3342ef579d1da
yqiu <120360845@qq.com>
2021-04-25 10:59:17 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='starduster',
+    version='alpha',
+    description='A dust extinction curve generator',
+    author="Yisheng Qiu",
+    author_email="hpc_yqiuu@163.com",
+    url='https://github.com/yqiuu/starduster',
+    packages=find_packages(),
+)
