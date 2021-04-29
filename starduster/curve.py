322fafc47e75e62b57ef59759d7fdaf979af548f
yqiu <120360845@qq.com>
2021-04-29 15:23:43 +0800

    Add ResNet and remove DualSequential

--- starduster/curve.py
+++ starduster/curve.py
@@ -8,29 +8,39 @@ from torch import nn
 __all__ = ["ExtinctionCurve", "EvaluatorCurve"]
 
 
-class DualSequential(nn.Module):
-    def __init__(self, n_out, layer_sizes):
+def create_sequential(layer_sizes):
+    layers = []
+    for i, k in zip(layer_sizes[:-1], layer_sizes[1:]):
+        layers.extend([nn.Linear(i, k), nn.Tanh()])
+    return nn.Sequential(*layers)
+
+
+class ResNet(nn.Module):
+    def __init__(self, layer_sizes, n_res):
+        assert layer_sizes[0] == layer_sizes[-1]
+
         super().__init__()
-        layers = []
-        for i, k in zip(layer_sizes[:-1], layer_sizes[1:]):
-            layers.extend([nn.Linear(i, k), nn.Tanh()])
-        self.seq = nn.Sequential(*layers)
-        self.lin_shape = nn.Linear(layer_sizes[-1], n_out)
-        self.lin_norm = nn.Linear(layer_sizes[-1], 1)
+        for i_res in range(n_res):
+            setattr(self, f'mlp{i_res}', create_sequential(layer_sizes))
+        self.n_res = n_res
 
 
     def forward(self, x):
-        x = self.seq(x)
-        z = self.lin_shape(x)
-        norm = nn.functional.softplus(self.lin_norm(x))
-        return z, norm
+        for i_res in range(self.n_res):
+            x = x + getattr(self, f'mlp{i_res}')(x)
+        return x
 
 
 class ExtinctionCurve(nn.Module):
-    def __init__(self, auto_encoder, layer_sizes):
+    def __init__(self,
+        auto_encoder, layer_sizes, res_layer_sizes, n_res
+    ):
         super().__init__()
         self._auto_encoder = (auto_encoder,)
-        self.dual_seq = DualSequential(auto_encoder.n_latent, layer_sizes)
+        self.resnet = ResNet(res_layer_sizes, n_res)
+        self.mlp = create_sequential(layer_sizes)
+        self.lin_shape = nn.Linear(layer_sizes[-1], auto_encoder.n_latent)
+        self.lin_norm = nn.Linear(layer_sizes[-1], 1)
 
     
     @property
@@ -39,7 +49,10 @@ class ExtinctionCurve(nn.Module):
 
 
     def forward(self, x):
-        z, norm = self.dual_seq(x)
+        x = self.resnet(x)
+        x = self.mlp(x)
+        z = self.lin_shape(x)
+        norm = nn.functional.softplus(self.lin_norm(x))
         k = self.auto_encoder.decoder(z)
         return k*norm
 
