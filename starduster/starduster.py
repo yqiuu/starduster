8df23866720a46548cca5a40cb59f84c3b2ad5ee
yqiuu <hpc_yqiuu@163.com>
2021-07-28 10:35:15 +0800

    Add DustAttenuation

--- starduster/starduster.py
+++ starduster/starduster.py
@@ -1,4 +1,4 @@
-from .modules import Monotonic, Unimodal, Smooth, create_mlp, PlankianMixture
+from .modules import Monotonic, Unimodal, Smooth, create_mlp
 
 import torch
 from torch import nn
@@ -37,4 +37,27 @@ class AttenuationCurve(nn.Module):
         y = y*self.norm(x)/torch.mean(y, dim=1)[:, None]
         return y
 
+
+class DustAttenuation(nn.Module):
+    def __init__(self, lookup, curve_disk, curve_bulge, l_ssp):
+        super().__init__()
+        self.lookup = lookup
+        self.curve_disk = curve_disk
+        self.curve_bulge = curve_bulge
+        self.register_buffer('l_ssp', l_ssp)
+
+
+    def forward(self, x_in):
+        x, sfh_disk, sfh_bulge = x_in
+        b2t = .5*(x[:, self.lookup['b_o_t']] + 1)[:, None] # Convert to range [0, 1]
+        x_disk = x[:, self.lookup['curve_disk_inds']]
+        x_bulge = x[:, self.lookup['curve_bulge_inds']]
+        l_disk = torch.matmul(sfh_disk, self.l_ssp)
+        l_bulge = torch.matmul(sfh_bulge, self.l_ssp)
+        trans_disk = torch.ones_like(l_disk)
+        trans_disk[:, self.lookup['slice_da']] = 10**(-.4*self.curve_disk(x_disk))
+        trans_bulge = torch.ones_like(l_bulge)
+        trans_bulge[:, self.lookup['slice_da']] = 10**(-.4*self.curve_bulge(x_bulge))
+        l_tot = l_disk*trans_disk*(1 - b2t) + l_bulge*trans_bulge*b2t
+        return l_tot
 
