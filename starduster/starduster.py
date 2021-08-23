05b1538a605920ce7708554cfee290c8aa3ef93d
yqiuu <hpc_yqiuu@163.com>
2021-08-23 10:28:26 +0800

    transform and recover can accept array of keys

--- starduster/starduster.py
+++ starduster/starduster.py
@@ -267,28 +267,44 @@ class Helper:
         return target
 
 
-    def transform(self, target, key):
-        target = self.get_item(target, key)
-        if key == 'theta':
-            target = torch.cos(np.pi/180.*target)
-        elif key == 'b_o_t':
-            pass
+    def transform(self, target, key=None):
+        if key is None:
+            key = list(self.header.keys())
         else:
-            log_min, log_max = self.header[key]
-            target = (torch.log10(target) - log_min)/(log_max - log_min)
-        target = 2*target - 1 # Convert from [0, 1] to [-1, 1]
-        return target
+            key = np.ravel(key)
+        output = torch.zeros([target.size(0), len(key)], dtype=target.dtype, device=target.device)
+        for i_k, k in enumerate(key):
+            arr = self.get_item(target, k)
+            if k == 'theta':
+                arr = torch.cos(np.pi/180.*arr)
+            elif k == 'b_to_t':
+                pass
+            else:
+                log_min, log_max = self.header[k]
+                arr = (torch.log10(arr) - log_min)/(log_max - log_min)
+            output[:, i_k] = arr
+        output = torch.squeeze(output)
+        output = 2*output - 1 # Convert from [0, 1] to [-1, 1]
+        return output
 
 
-    def recover(self, target, key):
-        target = self.get_item(target, key)
-        target = .5*(target + 1) # Convert from [-1, 1] to [0, 1]
-        if key == 'theta':
-            target = 180/np.pi*np.arccos(target)
-        elif key == 'b_o_t':
-            pass
+    def recover(self, target, key=None):
+        if key is None:
+            key = list(self.header.keys())
         else:
-            log_min, log_max = self.header[key]
-            target = 10**(target*(log_max - log_min) + log_min)
-        return target
+            key = np.ravel(key)
+        output = torch.zeros([target.size(0), len(key)], dtype=target.dtype, device=target.device)
+        for i_k, k in enumerate(key):
+            arr = self.get_item(target, k)
+            arr = .5*(arr + 1) # Convert from [-1, 1] to [0, 1]
+            if k == 'theta':
+                arr = 180/np.pi*np.arccos(arr)
+            elif k == 'b_to_t':
+                pass
+            else:
+                log_min, log_max = self.header[k]
+                arr = 10**(arr*(log_max - log_min) + log_min)
+            output[:, i_k] = arr
+        output = torch.squeeze(output)
+        return output
 
