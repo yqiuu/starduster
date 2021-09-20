e04ae06c12e8f71f11f12c51b5a5c12b2ed3944c
yqiuu <hpc_yqiuu@163.com>
2021-09-20 15:51:10 +0800

    Add partial_init and fix some bugs

--- starduster/parameter.py
+++ starduster/parameter.py
@@ -3,6 +3,22 @@ import torch
 from torch import nn
 
 
+def partial_init(cls):
+    class Wrapper:
+        def __init__(self, *args, **kwargs):
+            self.args = args
+            self.kwargs = kwargs
+            self.cls = cls
+
+
+        def init(self, arg_reserved):
+            return self.cls(arg_reserved, *self.args, **self.kwargs)
+
+    Wrapper.__name__ = cls.__name__
+    Wrapper.__doc__ = cls.__doc__
+    return Wrapper
+
+
 class ParameterSet(nn.Module):
     def __init__(self, bounds, params_default, free_inds):
         super().__init__()
@@ -32,58 +48,64 @@ class ParameterSet(nn.Module):
         return params
 
 
+@partial_init
+class GalaxyParameter(ParameterSet):
+    def __init__(self, helper, fixed_gp=None):
+        bounds = [(-1., 1.)]*len(helper.header)
+        params_default, free_inds = derive_default_params(helper.header.keys(), fixed_gp)
+        super().__init__(bounds, params_default, free_inds)
+
+
+@partial_init
+class DiscreteSFH(ParameterSet):
+    def __init__(self, lib_ssp, fixed_sfh=None):
+        bounds = [(0., 1.)]*lib_ssp.n_ssp
+        if fixed_sfh is None:
+            params_default = torch.zeros(lib_ssp.n_ssp, dtype=torch.float32)
+            free_inds = slice(0, lib_ssp.n_ssp)
+        else:
+            assert fixed_sfh.sum() == 1., "Star foramtion history should be normalised to one."
+            params_default = torch.ravel(fixed_sfh)
+            free_inds = ()
+        super().__init__(bounds, params_default, free_inds)
+
+
+@partial_init
 class InverseDistanceWeightedSFH(ParameterSet):
-    def __init__(self, lib_ssp):
-        bounds = [(0, 1)]*4
-        super().__init__(bounds, torch.zeros(4), slice(0, 4))
+    def __init__(self, lib_ssp, fixed_sfh=None):
         met_sol = 0.019
-        self.register_buffer('log_met', torch.log10(lib_ssp.met/met_sol)[:, None])
-        self.register_buffer('log_tau', torch.log10(lib_ssp.tau)[:, None])
+        log_met = torch.log10(lib_ssp.met/met_sol)[:, None]
+        log_tau = torch.log10(lib_ssp.tau)[:, None]
+        bounds = np.asarray([
+            (float(log_met[0]), float(log_met[-1])), (-2., 1.),
+            (float(log_tau[0]), float(log_tau[-1])), (-2., 1.)
+        ])
+        param_names = ['log_met', 's_met', 'log_tau', 's_tau']
+        params_default, free_inds = derive_default_params(param_names, fixed_sfh)
+        super().__init__(bounds, params_default, free_inds)
+        self.register_buffer('log_met', log_met)
+        self.register_buffer('log_tau', log_tau)
 
 
     def derive_full_params(self, params):
         eps = 1e-6
         log_met, s_met, log_tau, s_tau = params.T
-        inv = 1./(torch.abs(log_met - self.log_met)**s_met + eps)
+        inv = 1./(torch.abs(log_met - self.log_met)**(10**s_met) + eps)
         w_met = (inv/inv.sum(dim=0)).T
-        inv = 1./(torch.abs(log_tau - self.log_tau)**s_tau + eps)
+        inv = 1./(torch.abs(log_tau - self.log_tau)**(10**s_tau) + eps)
         w_tau = (inv/inv.sum(dim=0)).T
-        return w_met[:, :, None]*w_tau[:, None, :]
+        return torch.flatten(w_met[:, :, None]*w_tau[:, None, :], start_dim=1)
 
 
-class GalaxyParameter:
-    def __init__(self, fixed_gp=None):
-        if fixed_gp is None:
-            self.fixed_gp = {}
+def derive_default_params(param_names, fixed_params):
+    if fixed_params is None:
+        fixed_params = {}
+    params_default = torch.zeros(len(param_names))
+    free_inds = []
+    for i_k, key in enumerate(param_names):
+        if key in fixed_params:
+            params_default[i_k] = fixed_params[key]
         else:
-            self.fixed_gp = fixed_gp
-
-
-    def init(self, helper):
-        bounds = [(-1., 1.)]*len(helper.header)
-        params_default = torch.zeros(len(helper.header))
-        free_inds = []
-        for i_k, key in enumerate(helper.header):
-            if key in self.fixed_gp:
-                params_default[i_k] = self.fixed_gp[key]
-            else:
-                free_inds.append(i_k)
-        return ParameterSet(bounds, params_default, free_inds)
-
-
-class DiscreteSFH:
-    def __init__(self, fixed_sfh=None):
-        self.fixed_sfh = fixed_sfh
-
-
-    def init(self, lib_ssp):
-        bounds = [(0., 1.)]*lib_ssp.n_ssp
-        if self.fixed_sfh is None:
-            params_default = torch.zeros(lib_ssp.n_ssp, dtype=torch.float32)
-            free_inds = slice(0, lib_ssp.n_ssp)
-        else:
-            assert self.fixed_sfh.sum() == 1., "Star foramtion history should be normalised to one."
-            params_default = torch.ravel(self.fixed_sfh)
-            free_inds = ()
-        return ParameterSet(bounds, params_default, free_inds)
-
+            free_inds.append(i_k)
+    return params_default, free_inds
+           
