65c63ecc854ffdedf63453848ec5606445c3d709
yqiuu <hpc_yqiuu@163.com>
2021-09-27 13:22:53 +0800

    Add compute_parameter_summary

--- starduster/starduster.py
+++ starduster/starduster.py
@@ -150,6 +150,52 @@ class MultiwavelengthSED(nn.Module):
         self.detector.configure(filters=filters, z=z, distmod=distmod, ab_mag=ab_mag)
 
 
+    def compute_parameter_summary(self, params, print_summary=False):
+        gp, sfh_disk, sfh_bulge = self.adapter(params)
+        gp_0 = self.helper.recover_all(gp, torch)
+
+        theta = self.helper.get_item(gp_0, 'theta')
+        r_disk = self.helper.get_item(gp_0, 'r_disk')
+        r_bulge = self.helper.get_item(gp_0, 'r_bulge')
+        r_dust = r_disk*self.helper.get_item(gp_0, 'r_dust_to_rd')
+        m_dust = self.compute_m_dust(gp_0)
+        m_disk, m_bulge = self.compute_m_star(gp_0, sfh_disk, sfh_bulge, separate=True)
+        m_star = m_disk + m_bulge
+        # SFR over 10 Myr
+        sfr_10 = self.compute_sfr(gp_0, sfh_disk, sfh_bulge, time_scale=1e7, separate=False)
+        # SFR over 100 Myr
+        sfr_100 = self.compute_sfr(gp_0, sfh_disk, sfh_bulge, time_scale=1e8, separate=False)
+
+        names = [
+            'theta', 'r_disk', 'r_bulge', 'r_dust', 'm_dust',
+            'm_disk', 'm_bulge', 'm_star', 'sfr_10', 'sfr_100'
+        ]
+        # TODO: put units in somewhere more formal
+        units = {
+            'theta': '',
+            'r_disk': 'kpc',
+            'r_bulge': 'kpc',
+            'r_dust': 'kpc',
+            'm_dust': 'M_sol',
+            'm_disk': 'M_sol',
+            'm_bulge': 'M_sol',
+            'm_star': 'M_sol',
+            'sfr_10': 'M_sol/yr',
+            'sfr_100': 'M_sol/yr'
+        }
+        variables = locals()
+        summary = {key: variables[key] for key in names}
+
+        if print_summary:
+            for key, val in summary.items():
+                if key == 'theta':
+                    print("{}: {:.1f} {}".format(key, val[0], units[key]))
+                else:
+                    print("{}: {:.3e} {}".format(key, val[0], units[key]))
+
+        return summary
+
+
     def compute_m_dust(self, gp_0):
         r_disk = self.helper.get_item(gp_0, 'r_disk')
         r_dust_to_rd = self.helper.get_item(gp_0, 'r_dust_to_rd')
@@ -174,7 +220,7 @@ class MultiwavelengthSED(nn.Module):
         sfh_bulge = self.lib_ssp.sum_over_met(sfh_bulge)
 
         d_tau = torch.cumsum(self.lib_ssp.d_tau, dim=0)
-        idx = torch.argmin(torch.abs(d_tau - time_scale)) 
+        idx = torch.argmin(torch.abs(d_tau - time_scale))
         sfr_disk = sfh_disk[:, :idx+1].sum(dim=1)/d_tau[idx]
         sfr_bulge = sfh_bulge[:, :idx+1].sum(dim=1)/d_tau[idx]
 
