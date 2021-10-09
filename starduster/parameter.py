2ac05218d8d00b71f0c3833549e0ff780b47eb7f
yqiuu <hpc_yqiuu@163.com>
2021-10-09 14:59:24 +0800

    Simplify ParameterSet

--- starduster/parameter.py
+++ starduster/parameter.py
@@ -21,8 +21,11 @@ def partial_init(cls):
 
 
 class ParameterSet(nn.Module):
-    def __init__(self, bounds, params_default, free_inds, clip_bounds):
+    def __init__(self, param_names, fixed_params, bounds_default, bounds, clip_bounds):
         super().__init__()
+        self.param_names = param_names
+        params_default, free_inds = self._derive_default_params(param_names, fixed_params)
+        self._update_bounds(param_names, bounds_default, bounds)
         self.register_buffer('params_default', params_default)
         self.free_inds = free_inds
         if type(free_inds) is slice:
@@ -32,7 +35,7 @@ class ParameterSet(nn.Module):
         if self.input_size == 0:
             self.bounds = np.empty((0, 2), dtype=np.float)
         else:
-            bounds = np.asarray(bounds)[self.free_inds]
+            bounds = np.asarray(bounds_default)[free_inds]
             lbounds, ubounds = torch.tensor(bounds, dtype=torch.float32).T
             self.register_buffer('lbounds', lbounds)
             self.register_buffer('ubounds', ubounds)
@@ -71,28 +74,46 @@ class ParameterSet(nn.Module):
         return params
 
 
+    def _derive_default_params(self, param_names, fixed_params):
+        params_default = torch.zeros(len(param_names))
+        free_inds = []
+        for i_k, key in enumerate(param_names):
+            if key in fixed_params:
+                params_default[i_k] = fixed_params[key]
+            else:
+                free_inds.append(i_k)
+        return params_default, free_inds
+
+
+    def _update_bounds(self, param_names, bounds_default, bounds):
+        if bounds is None:
+            return
+
+        for i_k, key in enumerate(param_names):
+            if key in bounds:
+                lb, ub = bounds_default[i_k]
+                lb_new, ub_new = bounds[key]
+                if lb_new >= lb and ub_new <= ub:
+                    bounds_default[i_k] = (lb_new, ub_new)
+                else:
+                    msg = "The bounds of {} should be within [{:.2f}, {:.2f}]".format(key, lb, ub)
+                    raise ValueError(msg)
+
+
 @partial_init
 class GalaxyParameter(ParameterSet):
     def __init__(self, helper, bounds=None, clip_bounds=True, **fixed_params):
-        bounds_default = [(-1., 1.)]*len(helper.header)
         param_names = helper.header.keys()
-        params_default, free_inds = derive_default_params(param_names, fixed_params)
-        update_bounds(param_names, bounds_default, bounds)
-        super().__init__(bounds_default, params_default, free_inds, clip_bounds)
+        bounds_default = [(-1., 1.)]*len(helper.header)
+        super().__init__(param_names, fixed_params, bounds_default, bounds, clip_bounds)
 
 
 @partial_init
 class DiscreteSFH(ParameterSet):
-    def __init__(self, lib_ssp, fixed_sfh=None, clip_bounds=True):
-        bounds = [(0., 1.)]*lib_ssp.n_ssp
-        if fixed_sfh is None:
-            params_default = torch.zeros(lib_ssp.n_ssp, dtype=torch.float32)
-            free_inds = slice(0, lib_ssp.n_ssp)
-        else:
-            assert fixed_sfh.sum() == 1., "Star foramtion history should be normalised to one."
-            params_default = torch.ravel(fixed_sfh)
-            free_inds = ()
-        super().__init__(bounds, params_default, free_inds, clip_bounds)
+    def __init__(self, lib_ssp, bounds=None, clip_bounds=True, **fixed_params):
+        param_names = [f'sfr_{i_sfr}' for i_sfr in range(lib_ssp.n_ssp)]
+        bounds_default = [(0., 1.)]*lib_ssp.n_ssp
+        super().__init__(param_names, fixed_params, bounds_default, bounds, clip_bounds)
 
 
 @partial_init
@@ -116,15 +137,13 @@ class DiscreteSFR_InterpolatedMet(ParameterSet):
         log_met_min = float(log_met[0])
         log_met_max = float(log_met[-1])
         if uni_met:
-            bounds_default = np.array([(0., 1.)]*n_sfr + [(log_met_min, log_met_max)])
             param_names = [f'sfr_{i_sfr}' for i_sfr in range(n_sfr)] + ['log_met']
+            bounds_default = np.array([(0., 1.)]*n_sfr + [(log_met_min, log_met_max)])
         else:
-            bounds_default = np.array([(0., 1.)]*n_sfr + [(log_met_min, log_met_max)]*n_sfr)
             param_names = [f'sfr_{i_sfr}' for i_sfr in range(n_sfr)] \
                 + [f'log_met_{i_sfr}' for i_sfr in range(n_sfr)]
-        params_default, free_inds = derive_default_params(param_names, fixed_params)
-        update_bounds(param_names, bounds_default, bounds)
-        super().__init__(bounds_default, params_default, free_inds, clip_bounds)
+            bounds_default = np.array([(0., 1.)]*n_sfr + [(log_met_min, log_met_max)]*n_sfr)
+        super().__init__(param_names, fixed_params, bounds_default, bounds, clip_bounds)
         self.register_buffer('log_met', log_met)
         self.n_sfr = n_sfr
 
@@ -161,14 +180,12 @@ class InverseDistanceWeightedSFH(ParameterSet):
         met_sol = 0.019
         log_met = torch.log10(lib_ssp.met/met_sol)[:, None]
         log_tau = torch.log10(lib_ssp.tau)[:, None]
+        param_names = ['log_met', 's_met', 'log_tau', 's_tau']
         bounds_default = np.asarray([
             (float(log_met[0]), float(log_met[-1])), (-2., 1.),
             (float(log_tau[0]), float(log_tau[-1])), (-2., 1.)
         ])
-        param_names = ['log_met', 's_met', 'log_tau', 's_tau']
-        params_default, free_inds = derive_default_params(param_names, fixed_params)
-        update_bounds(param_names, bounds_default, bounds)
-        super().__init__(bounds_default, params_default, free_inds, clip_bounds)
+        super().__init__(param_names, fixed_params, bounds_default, bounds, clip_bounds)
         self.register_buffer('log_met', log_met)
         self.register_buffer('log_tau', log_tau)
 
@@ -181,30 +198,4 @@ class InverseDistanceWeightedSFH(ParameterSet):
         inv = 1./(torch.abs(log_tau - self.log_tau)**(10**s_tau) + eps)
         w_tau = (inv/inv.sum(dim=0)).T
         return torch.flatten(w_met[:, :, None]*w_tau[:, None, :], start_dim=1)
-
-
-def derive_default_params(param_names, fixed_params):
-    params_default = torch.zeros(len(param_names))
-    free_inds = []
-    for i_k, key in enumerate(param_names):
-        if key in fixed_params:
-            params_default[i_k] = fixed_params[key]
-        else:
-            free_inds.append(i_k)
-    return params_default, free_inds
-
-
-def update_bounds(param_names, bounds_default, bounds):
-    if bounds is None:
-        return
-
-    for i_k, key in enumerate(param_names):
-        if key in bounds:
-            lb, ub = bounds_default[i_k]
-            lb_new, ub_new = bounds[key]
-            if lb_new >= lb and ub_new <= ub:
-                bounds_default[i_k] = (lb_new, ub_new)
-            else:
-                msg = "The bounds of {} should be within [{:.2f}, {:.2f}]".format(key, lb, ub)
-                raise ValueError(msg)
 
