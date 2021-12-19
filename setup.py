2960b4e38e1243214b23d1b5ffac6f0699b54ca8
yqiuu <hpc_yqiuu@163.com>
2021-12-19 15:10:41 +0800

    Add install_requires

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+    'astropy>=4.2',
+    'numpy>=1.17.0',
+    'astro-sedpy>=0.2.0',
+    'torch>=1.9.0',
+    'tqdm',
+]
 
 setup(
     name='starduster',
@@ -8,5 +15,6 @@ setup(
     author="Yisheng Qiu",
     author_email="hpc_yqiuu@163.com",
     url='https://github.com/yqiuu/starduster',
+    install_requires=install_requires,
     packages=find_packages(),
 )
