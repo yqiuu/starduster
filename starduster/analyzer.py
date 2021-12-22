0422d55309fb00add4667cb8b69f95f9d39b784f
yqiu <120360845@qq.com>
2021-12-22 10:56:27 +0800

    Rewrite compute_parameter_summary

--- starduster/analyzer.py
+++ starduster/analyzer.py
@@ -2,8 +2,28 @@ from .utils import units, constants, accept_reject
 from .sed_model import MultiwavelengthSED
 from .selector import sample_from_selector
 
+from functools import update_wrapper
+
 import torch
 import numpy as np
+
+
+def register_calculator(name, input_type, is_separable):
+    def wrapper(func):
+        return update_wrapper(PropertyCalculator(func, name, input_type, is_separable), func)
+    return wrapper
+
+
+class PropertyCalculator:
+    def __init__(self, func, name, input_type, is_separable):
+        self.func = func
+        self.name = name
+        self.input_type = input_type
+        self.is_separable = is_separable
+
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
 
 
 class Analyzer:
@@ -18,84 +38,66 @@ class Analyzer:
         self.sed_model = sed_model
         self.helper = self.sed_model.helper
         self.lib_ssp = self.sed_model.adapter.lib_ssp
+        self._collect_calculators()
 
 
-    def compute_parameter_summary(self, params, log_scale=False, print_summary=False):
-        """Compute some selected physical parameters.
-
-        Parameters
-        ----------
-        params : tensor
-            Scaled parameters.
-        log_scale : bool
-            If True, transform the parameters into logarithmic space.
-        print_summary : bool
-            If True, print the first element of the selected parameters.
-
-        Returns
-        -------
-        summary : tensor
-            Selected parameters.
-        """
-        gp_0, sfh_disk_0, sfh_bulge_0 = self.recover_params(params)
-
-        theta = self.helper.get_item(gp_0, 'theta')
-        r_disk = self.helper.get_item(gp_0, 'r_disk')
-        r_bulge = self.helper.get_item(gp_0, 'r_bulge')
-        r_dust = r_disk*self.helper.get_item(gp_0, 'r_dust_to_rd')
-        l_norm = self.helper.get_item(gp_0, 'l_norm')
-        b_to_t = self.helper.get_item(gp_0, 'b_to_t')
-        m_dust = self.compute_m_dust(gp_0)
-        m_disk, m_bulge = self.compute_m_star(gp_0, sfh_disk_0, sfh_bulge_0, separate=True)
-        m_star = m_disk + m_bulge
-        # SFR over 100 Myr
-        sfr = self.compute_sfr(gp_0, sfh_disk_0, sfh_bulge_0, time_scale=1e8)
-        met = self.compute_mass_weighted_met(gp_0, sfh_disk_0, sfh_bulge_0)
-        #sfr_disk, sfr_bulge = \
-        #    self.compute_sfr(gp_0, sfh_disk_0, sfh_bulge_0, time_scale=1e8, separate=True)
-        ##
-        #met_disk, met_bulge = \
-        #    self.compute_mass_weighted_met(gp_0, sfh_disk_0, sfh_bulge_0, separate=True)
-
-        names = [
-            'theta', 'r_disk', 'r_bulge', 'r_dust', 'l_norm', 'b_to_t',
-            'm_dust', 'm_disk', 'm_bulge', 'm_star', 'sfr', 'met'
-        ]
-        variables = locals()
-        summary = {}
-        for key in names:
-            if key == 'theta' or key == 'b_to_t':
-                summary[key] = variables[key]
+    def list_available_properties(self):
+        prop_names = []
+        for name, calc in self._calculators.items():
+            if calc.is_separable:
+                prop_names.extend([name, name + '_disk', name + '_bulge'])
             else:
-                if log_scale:
-                    summary[key] = torch.log10(variables[key])
+                prop_names.append(name)
+        return prop_names
+
+
+    def compute_property_summary(self, params, prop_names):
+        gp_0, sfh_disk_0, sfh_bulge_0 = self.recover_params(params)
+        output = {}
+        for name in prop_names:
+            if name in output:
+                continue
+
+            if name.endswith('_disk'):
+                name_disk = name
+                name_bulge = name.replace('_disk', '_bulge')
+                name = name.replace('_disk', '')
+            elif name.endswith('_bulge'):
+                name_disk = name.replace('_bulge', '_disk')
+                name_bulge = name
+                name = name.replace('_bulge', '')
+            else:
+                name_disk = None
+                name_bulge = None
+
+            calc = self._calculators[name]
+            if calc.input_type == 'recovered_gp':
+                args = gp_0,
+            elif calc.input_type == 'recovered_params':
+                args = gp_0, sfh_disk_0, sfh_bulge_0
+            elif calc.input_type == 'scaled_params':
+                args = params,
+            else:
+                raise ValueError
+
+            if name_disk is None:
+                output[name] = calc(self, *args)
+            else:
+                if calc.is_separable:
+                    # Always store the properties of both the disk and bulge
+                    # components. The unwanted property will be deleted later.
+                    output[name_disk], output[name_bulge] = calc(self, *args, separate=True)
                 else:
-                    summary[key] = variables[key]
+                    raise ValueError("{} is not separable.".format(name))
 
-        if print_summary:
-            for key, val in summary.items():
-                if 'sfr' in key:
-                    unit_name = 'sfr'
-                elif 'met' in key:
-                    unit_name = 'met'
-                else:
-                    unit_name = key
-                msg = (key, val[0], getattr(units, unit_name))
-                if key == 'theta':
-                    print("{}: {:.1f} {}".format(*msg))
-                elif key == 'b_to_t':
-                    print("{}: {:.2f} {}".format(*msg))
-                elif 'met' in key:
-                    print("{}: {:.3f} {}".format(*msg))
-                else:
-                    if log_scale:
-                        print("{}: {:.2f} {}".format(*msg))
-                    else:
-                        print("{}: {:.2e} {}".format(*msg))
-
-        return summary
+        for out_name in list(output.keys()):
+            if out_name not in prop_names:
+                del output[out_name]
+        
+        return output
 
 
+    @register_calculator('l_bol', 'scaled_params', is_separable=False)
     def compute_l_bol(self, params):
         """Compute bolometic luminosity.
 
@@ -114,6 +116,7 @@ class Analyzer:
         return torch.trapz(lum, torch.log(self.sed_model.lam))
 
 
+    @register_calculator('m_dust', 'recovered_gp', is_separable=False)
     def compute_m_dust(self, gp_0):
         """Compute dust mass.
 
@@ -134,6 +137,7 @@ class Analyzer:
         return den_dust*(2*np.pi*r_dust*r_dust)
 
 
+    @register_calculator('m_star', 'recovered_params', is_separable=True)
     def compute_m_star(self, gp_0, sfh_disk_0, sfh_bulge_0, separate=False):
         """Compute stellar mass.
 
@@ -162,6 +166,7 @@ class Analyzer:
             return m_disk + m_bulge
 
 
+    @register_calculator('sfr', 'recovered_params', is_separable=True)
     def compute_sfr(self, gp_0, sfh_disk_0, sfh_bulge_0, time_scale=1e8, separate=False):
         """Compute SFR.
 
@@ -198,6 +203,7 @@ class Analyzer:
             return sfr_disk + sfr_bulge
 
 
+    @register_calculator('mwa', 'recovered_params', is_separable=True)
     def compute_mass_weighted_age(self, gp_0, sfh_disk_0, sfh_bulge_0, separate=False):
         """Compute mass weighted age.
 
@@ -232,6 +238,7 @@ class Analyzer:
             return compute_average(tau, sfh_disk_0 + sfh_bulge_0)
 
 
+    @register_calculator('mwm', 'recovered_params', is_separable=True)
     def compute_mass_weighted_met(self, gp_0, sfh_disk_0, sfh_bulge_0, separate=False):
         """Compute mass weighted metallicity.
 
@@ -324,4 +331,13 @@ class Analyzer:
         sfh_bulge_0 = recover(sfh_bulge, l_norm*b_to_t)
 
         return sfh_disk_0, sfh_bulge_0
+
+
+    def _collect_calculators(self):
+        calculators = {}
+        for name in dir(self):
+            attr = getattr(self, name)
+            if isinstance(attr, PropertyCalculator):
+                calculators[attr.name] = attr
+        self._calculators = calculators
 
