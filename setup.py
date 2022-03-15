a33664e1860b450e9df4e8f4da14de2ddd0a78e1
yqiuu <hpc_yqiuu@163.com>
2022-03-15 12:28:07 +0800

    Add version.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 description = \
-    "A multi-wavelength SED model based on radiative transfer simulations and deep learning"
+"""A multi-wavelength SED model based on radiative transfer simulations and
+deep learning"""
+
 install_requires = [
     'astropy>=3',
     'numpy>=1.17',
@@ -10,9 +12,12 @@ install_requires = [
     'tqdm',
 ]
 
+# Get version
+exec(open('starduster/version.py', 'r').read())
+
 setup(
     name='starduster',
-    version='beta',
+    version=__version__,
     author="Yisheng Qiu",
     author_email="hpc_yqiuu@163.com",
     description=description,
