b310b3c0951e5dd776b9970a22a3e73c9aba304c
yqiuu <hpc_yqiuu@163.com>
2022-04-22 11:47:33 +0800

    Enable share_pn option

--- starduster/adapter.py
+++ starduster/adapter.py
@@ -29,6 +29,7 @@ class Adapter(nn.Module, Configurable):
             pn_gp=GalaxyParameter(),
             pn_sfh_disk=VanillaGrid(),
             pn_sfh_bulge=VanillaGrid(),
+            share_pn='none',
             flat_input=False,
             check_sfh_norm=True,
         )
@@ -46,6 +47,10 @@ class Adapter(nn.Module, Configurable):
         for name, pn in self.get_config().items():
             if name.startswith('pn'):
                 pn.enable(self.helper, self.lib_ssp)
+                if self.share_pn == 'all' and name != 'pn_gp':
+                    continue
+                if self.share_pn == 'sfh' and name == 'pn_sfh_bulge':
+                    continue
                 free_shape.append(pn.input_size)
                 param_names.extend(pn.param_names)
                 bounds.append(pn.bounds)
@@ -55,33 +60,36 @@ class Adapter(nn.Module, Configurable):
         self.bounds = np.vstack(bounds)
 
 
-    def forward(self, *args, check_bounds=False):
-        if self.flat_input:
-            params = torch.as_tensor(args[0], dtype=torch.float32, device=self.device)
-            params = torch.atleast_2d(params)
-            gp, sfh_disk, sfh_bulge = torch.split(params, self.free_shape, dim=-1)
+    def forward(self, *args, check_selector=False):
+        if self.share_pn == 'all':
+            gp, sfh_disk, sfh_bulge = self.pn_gp(*args)
         else:
-            gp, sfh_disk, sfh_bulge = args
+            if self.flat_input:
+                params = torch.atleast_2d(args[0])
+                if self.share_pn == 'sfh':
+                    gp, sfh_disk = torch.split(params, self.free_shape, dim=-1)
+                    sfh_bulge = sfh_disk
+                else:
+                    gp, sfh_disk, sfh_bulge = torch.split(params, self.free_shape, dim=-1)
+            else:
+                if self.share_pn == 'sfh':
+                    gp, sfh_disk = args
+                    sfh_bulge = sfh_disk
+                else:
+                    gp, sfh_disk, sfh_bulge = args
+            gp = self.pn_gp(gp)
+            sfh_disk = self.pn_sfh_disk(sfh_disk)
+            sfh_bulge = self.pn_sfh_bulge(sfh_bulge)
 
-        if check_bounds:
-            # Check if all input parameters are within the bounds
-            is_out = torch.full((gp.size(0),), False, device=self.device)
-            for val, pn in zip(
-                [gp, sfh_disk, sfh_bulge],
-                [self.pn_gp, self.pn_sfh_disk, self.pn_sfh_bulge]
-            ):
-                is_out |= pn.check_bounds(val)
-            # Apply the parameterisations
-            gp, sfh_disk, sfh_bulge = self._apply_pn(gp, sfh_disk, sfh_bulge)
-            # Check if all parameters are in the effective region
-            # -Assume helper of selector_disk and selector_bulge are the same
-            helper = self.selector_disk.helper
-            is_out |= ~self.selector_disk.select(helper.get_item(gp, 'curve_disk_inds'))
-            is_out |= ~self.selector_bulge.select(helper.get_item(gp, 'curve_bulge_inds'))
-            #
-            return gp, sfh_disk, sfh_bulge, is_out
+        if self.check_sfh_norm:
+            msg = "Star formation history must be normalised to one."
+            assert torch.allclose(sfh_disk.sum(dim=-1), torch.tensor(1.), atol=1e-5), msg
+            assert torch.allclose(sfh_bulge.sum(dim=-1), torch.tensor(1.), atol=1e-5), msg
+
+        if check_selector:
+            return gp, sfh_disk, sfh_bulge, self._eval_selector(gp)
         else:
-            return self._apply_pn(gp, sfh_disk, sfh_bulge)
+            return gp, sfh_disk, sfh_bulge
 
 
     @property
@@ -89,15 +97,8 @@ class Adapter(nn.Module, Configurable):
         return self._device.device
 
 
-    def _apply_pn(self, gp, sfh_disk, sfh_bulge):
-        gp = self.pn_gp(gp)
-        sfh_disk = self.pn_sfh_disk(sfh_disk)
-        sfh_bulge = self.pn_sfh_bulge(sfh_bulge)
-
-        if self.check_sfh_norm:
-            msg = "Star formation history must be normalised to one."
-            assert torch.allclose(sfh_disk.sum(dim=-1), torch.tensor(1.), atol=1e-5), msg
-            assert torch.allclose(sfh_bulge.sum(dim=-1), torch.tensor(1.), atol=1e-5), msg
-
-        return gp, sfh_disk, sfh_bulge
+    def _eval_selector(self, gp):
+        p_in_disk = self.selector_disk(self.helper.get_item(gp, 'curve_disk_inds'))
+        p_in_bulge = self.selector_bulge(self.helper.get_item(gp, 'curve_bulge_inds'))
+        return F.logsigmoid(p_in_disk), F.logsigmoid(p_in_bulge)
 
